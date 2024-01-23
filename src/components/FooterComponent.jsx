7e3b764b837a2d6d0caabcdc798f4c0234e7c160
macbook pro <macbookpro@192.168.1.19>
2024-01-23 22:51:18 +0000

    final app 23-01-2023 22:51

--- src/components/FooterComponent.jsx
+++ src/components/FooterComponent.jsx
@@ -21,39 +21,16 @@ export default function FooterComponent() {
             <div class="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
               <div>
                 <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
-                  Resources
-                </h2>
-                <ul class="text-gray-500 dark:text-gray-400 font-medium">
-                  <li class="mb-4">
-                    <a href="" class="hover:underline">
-                      Flowbite
-                    </a>
-                  </li>
-                  <li>
-                    <a href="" class="hover:underline">
-                      Tailwind CSS
-                    </a>
-                  </li>
-                </ul>
-              </div>
-              <div>
-                <h2 class="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
                   Follow us
                 </h2>
                 <ul class="text-gray-500 dark:text-gray-400 font-medium">
                   <li class="mb-4">
-                    <a
-                      href=""
-                      class="hover:underline "
-                    >
+                    <a href="" class="hover:underline ">
                       Github
                     </a>
                   </li>
                   <li>
-                    <a
-                      href=""
-                      class="hover:underline"
-                    >
+                    <a href="" class="hover:underline">
                       Discord
                     </a>
                   </li>
